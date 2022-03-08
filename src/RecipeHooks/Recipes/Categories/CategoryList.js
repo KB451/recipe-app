e349c15667e633f456e12b54f3545eec95edfd5d
Kerry B <learn33333@gmail.com>
2022-03-08 15:04:17 -0500

    create function in recipeform component that is passed to categorylist comopnent so a specific category can be targeted

--- src/RecipeHooks/Recipes/Categories/CategoryList.js
+++ src/RecipeHooks/Recipes/Categories/CategoryList.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-function CategoryList() {
+function CategoryList({selectCtgy}) {
     const categories = [
-        {name: "--Select Category--"},
-        {name: "Pasta", id: uuidv4()},
-        {name: "Seafood", id: uuidv4()},
-        {name: "Appetizers", id: uuidv4()}
+        {name: "Appetizers", id: uuidv4()},
+        {name: "Soups & Salads", id: uuidv4()},
+        {name: "Main Dishes: Meat", id: uuidv4()},
+        {name: "Main Dishes: Vegetarian", id: uuidv4()},
+        {name: "Sides", id: uuidv4()},
+        {name: "Desserts", id: uuidv4()},
+        {name: "Beverages", id: uuidv4()},
     ]
-    const [ctgy, setCtgy] = useState(categories)
-
     return (
         // <div>
-            <select>
-                {ctgy.map(c => (                    
+            <select
+            onChange={e => selectCtgy(e.target.value)}
+            >
+                <option>--Select Category--</option>
+                {categories.map(c => (                    
                     <option value={c.name}>{c.name}</option>                                
                 ))}  
             </select> 
