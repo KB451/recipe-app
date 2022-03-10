3f8e1f4c550278306e86d8113699f19b7e6fcc54
Kerry B <learn33333@gmail.com>
2022-03-10 08:24:04 -0500

    update so user can filter recipes by category

--- src/RecipeHooks/Recipes/RecipeList.js
+++ src/RecipeHooks/Recipes/RecipeList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from 'uuid'
 import TitleInput from "./Inputs/TitleInput"
@@ -12,6 +12,8 @@ THAT DISPLAYS EITHER A LIST OF RECIPES OR A FORM TO ADD NEW ONES */
 function RecipeList({recipes, setRecipes}) {  
     //INPUT UPDATES AS USER SEARCHES OR ADDS A NEW TITLE TO RECIPE   
     const [title, setTitle, reset] = TitleInput("")
+    //FUNCTION UPDATES RECIPE CATEGORY WHEN USER SELECTS A CATEGORY NAME
+    const [ctgy, setCtgy] = useState("-- Select Category --")
     //FUNCTION TOGGLES BETWEEN FORM TO CREATE NEW RECIPE AND THE LIST OF RECIPES ALREADY CREATED
     const [isForm, toggleForm] = UseToggle()   
 
@@ -26,6 +28,12 @@ function RecipeList({recipes, setRecipes}) {
     //FUNCTION FILTERS OUT RECIPE FROM "recipes" 
     const removeRecipe = id => setRecipes(recipes.filter(r => r.id !== id))
 
+    //FUNCTION CALLS "setCtgy" TO RESET CATEGORY LIST AND CALLS "toggleForm" WHEN USER CLICKS ON ADD ICON
+    const resetListAndToggle = () => {
+        setCtgy("-- Select Category --")
+        toggleForm(isForm)
+    }
+
     //VARIABLE WILL EITHER DISPLAY FORM TO CREATE NEW RECIPE OR LIST OF RECIPES   
     let displayRecipeList;
     if (isForm) {
@@ -33,6 +41,7 @@ function RecipeList({recipes, setRecipes}) {
         displayRecipeList = (            
             <RecipeForm
             name={title}
+            ctgyName={ctgy}
             // PASSES FUNCTION "addRecipe" TO FORM SO USER INPUT CAN BE ADDED TO "recipes" DATA
             add={addRecipe}
             toggleHome={toggleForm}
@@ -50,18 +59,24 @@ function RecipeList({recipes, setRecipes}) {
                         {/* USER INPUT TO SEARCH OR ADD A RECIPE TITLE */}
                         <input type="text" placeholder="search or add recipe" name="title" value={title} onChange={setTitle}></input>
                         {/* BUTTON TOGGLES BETWEEN DISPLAYING FORM AND USER INPUT / LIST OF RECIPE TITLES */}
-                        <button className="iconButton" onClick={() => {toggleForm(isForm)}}><i className="fas fa-plus add"/></button>                    
+                        <button className="iconButton" onClick={() => {resetListAndToggle()}}><i className="fas fa-plus add"/></button>                    
                     </div>
-                    <div>
-                        <CategoryList />
+
+                    {/* LETS USER SELECT RECIPE CATEGORY */}
+                    <div className="category-container">
+                        <CategoryList
+                        ctgyName={ctgy}
+                        selectCtgy={setCtgy} 
+                        />
+                        <button className="iconButton-ctgy"><i className="fas fa-ellipsis-h ctgy-icon"></i></button>
                     </div>
                     
                     {/* IF "recipes" ARRAY HAS NOTHING IN IT, MESSAGE DISPLAYS */}
                     {recipes.length <= 0 ? <p>no recipes have been added</p> : ""}  
                     
-                    <div className="recipeTitle-container">
-                    {/* FILTERS "recipes" AS USER TYPES, TO DISPLAY A RECIPE TITLE THAT MIGHT MATCH SEARCH */}
-                        {recipes.filter(r => r.title.toLowerCase().includes(title.toLowerCase())).map(r => (
+                    <div className="recipeTitle-container">                   
+                        {/* FILTERS "recipes" SO THAT AS USER TYPES, A TITLE DISPLAYS IF THERE IS A MATCH */}
+                        {(ctgy === "-- Select Category --") && recipes.filter(r => r.title.toLowerCase().includes(title.toLowerCase())).map(r => (
                             <div className="recipeTitle-content" key={r.id}>
                                 {/* RECIPE TITLE IS TURNED INTO A LINK THAT USER CLICKS TO SEE RECIPE DETAILS */}
                                 <Link className="link titleLink xs" to={`/recipe/${r.id}`}>
@@ -70,7 +85,19 @@ function RecipeList({recipes, setRecipes}) {
                                 {/* CALLS FUNCTION TO DELETE A RECIPE WHEN CLICKED */}
                                 <button className="iconButton" onClick={() => removeRecipe(r.id)}><i className="fas fa-trash-alt trash"/></button>
                             </div>                                                                         
-                        ))}   
+                        ))}  
+                        
+                        {/* FILTERS "recipes" BY CATEGORY AND TITLE */}
+                        {(ctgy !==  "-- Select Category --") && recipes.filter(r => r.category === ctgy && r.title.toLowerCase().includes(title.toLowerCase())).map(r => (
+                        <div className="recipeTitle-content" key={r.id}>
+                            {/* RECIPE TITLE IS TURNED INTO A LINK THAT USER CLICKS TO SEE RECIPE DETAILS */}
+                            <Link className="link titleLink xs" to={`/recipe/${r.id}`}>
+                                <h3>{r.title}</h3>                        
+                            </Link> 
+                            {/* CALLS FUNCTION TO DELETE A RECIPE WHEN CLICKED */}
+                            <button className="iconButton" onClick={() => removeRecipe(r.id)}><i className="fas fa-trash-alt trash"/></button>
+                        </div>                                                                         
+                        ))} 
                     </div>                     
                 </div>  
             </div>
@@ -80,3 +107,5 @@ function RecipeList({recipes, setRecipes}) {
 }
 
 export default RecipeList;
+
+
