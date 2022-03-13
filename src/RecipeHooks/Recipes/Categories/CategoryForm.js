91538c396adc0518cb3c51c765bf4ba49fc2cc50
Kerry B <learn33333@gmail.com>
2022-03-13 11:54:34 -0400

    update categorymenu component and categoryform so user can add categories to the dropdown menu

--- src/RecipeHooks/Recipes/Categories/CategoryForm.js
+++ src/RecipeHooks/Recipes/Categories/CategoryForm.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
+import UseInput from "../Inputs/UseInput";
 
-function CategoryForm({ctgyName, editMode, toggle, updateCtgy, addNewCtgy}) {    
-    const [ctgy, setCtgy] = useState(editMode ? ctgyName : "")
+function CategoryForm({ctgyName, editMode, toggle, toggleMenuBar, updateCtgy, addNewCtgy}) {    
+    const [ctgy, setCtgy, reset] = UseInput(editMode ? {category: ctgyName} : {category: ""})
+   
     return (
         <div>
             <form onSubmit={e => {
                 e.preventDefault();
-                editMode ? updateCtgy(ctgy) : addNewCtgy(ctgy);                
+                editMode ? updateCtgy(ctgy) : addNewCtgy(ctgy);   
+                reset({category: ""})            
                 toggle(true);
+                toggleMenuBar(true)
             }}           
             >
-                <input type="text" placeholder="add category" value={ctgy} onChange={setCtgy}></input>
+                <input type="text" placeholder="add category" name="category" value={ctgy.category} onChange={setCtgy}></input>
                 <button>save</button>
             </form>
         </div>
--- src/RecipeHooks/Recipes/Categories/CategoryMenu.js
+++ src/RecipeHooks/Recipes/Categories/CategoryMenu.js
@@ -1,20 +1,39 @@
 import React, {useState} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import UseLocalStorage from "../Inputs/UseLocalStorage";
-import CategoryForm from "./CategoryForm"
+import UseToggle from "../Inputs/UseToggle";
+import CategoryForm from "./CategoryForm";
 
-function CategoryMenu({selectCtgy, menuMode, toggleMenu}) {
+function CategoryMenu({ctgyName, selectCtgy, menuMode, toggleMenu}) {
     //VARIABLE ASSIGNED ARRAY WITH INITIAL VALUES OF RECIPE CATEGORIES THAT USER CAN CHOOSE FROM 
     const ctgyList = [
-        {name: "Appetizers", id: uuidv4()},
-        {name: "Soups & Salads", id: uuidv4()},
-        {name: "Main Dishes: Meat", id: uuidv4()},
-        {name: "Main Dishes: Vegetarian", id: uuidv4()},
-        {name: "Sides", id: uuidv4()},
-        {name: "Desserts", id: uuidv4()},
-        {name: "Beverages", id: uuidv4()},
+        {category: "Appetizers", id: uuidv4()},
+        {category: "Soups & Salads", id: uuidv4()},
+        {category: "Main Dishes: Meat", id: uuidv4()},
+        {category: "Main Dishes: Vegetarian", id: uuidv4()},
+        {category: "Sides", id: uuidv4()},
+        {category: "Desserts", id: uuidv4()},
+        {category: "Beverages", id: uuidv4()},
     ]
     const [categories, setCategories] = UseLocalStorage("categories", ctgyList)
+    
+
+    const [hideMenuBar, toggleHideMenuBar] = UseToggle();
+    const [isAdd, toggleIsAdd] = UseToggle();
+    const [isEdit, toggleIsEdit] = UseToggle();
+
+    const hideMenu = (toggleMenuBar) => {
+        toggleHideMenuBar();
+        toggleMenuBar()
+    }
+
+    const addCtgy = (newCtgy) => {
+        console.log(newCtgy)
+        setCategories([...categories, {category: newCtgy.category, id: uuidv4()}])
+        console.log(categories)
+        console.log(newCtgy.category)
+    }
+    
     const displayCategories = (
         <div className="CategoryList-container">
             <select
@@ -22,7 +41,7 @@ function CategoryMenu({selectCtgy, menuMode, toggleMenu}) {
             > 
                 <option value="-- Select Category --">-- Select Category --</option>
                 {categories.map(c => (                    
-                    <option value={c.name}>{c.name}</option>                                
+                    <option key={c.id} value={c.category}>{c.category}</option>                                
                 ))}                          
             </select> 
         </div> 
@@ -32,16 +51,36 @@ function CategoryMenu({selectCtgy, menuMode, toggleMenu}) {
             <div>
                 {!menuMode && displayCategories}
             </div> 
-                {menuMode && 
+
+                {menuMode && !hideMenuBar &&
                     <div>
                         {displayCategories}
                         <button onClick={() => toggleMenu(true)}><i className="fas fa-newspaper"></i></button>                 
-                        <button className="iconButton" ><i className="fas fa-plus add"/></button> 
-                        <button className="iconButton" ><i className="fas fa-pencil-alt edit"/></button>  
+                        <button className="iconButton" onClick={() => hideMenu(toggleIsAdd)}><i className="fas fa-plus add"/></button> 
+                        <button className="iconButton" onClick={() => hideMenu(toggleIsEdit)}><i className="fas fa-pencil-alt edit"/></button>  
                         <button className="iconButton"><i className="fas fa-trash-alt trash"/></button> 
                     </div>}
 
-               
+                {isAdd && hideMenuBar && <CategoryForm
+                        toggle={toggleIsAdd}
+                        toggleMenuBar={toggleHideMenuBar}
+                        editMode={isEdit}
+                        addNewCtgy={addCtgy}
+                        ctgyName={ctgyName}
+                        />}
+                
+                {isEdit && hideMenuBar &&
+                    <div>
+                        {displayCategories}
+                        <CategoryForm
+                        ctgyName={ctgyName}
+                        editMode={isEdit}
+                        toggle={toggleIsEdit}
+                        toggleMenuBar={toggleHideMenuBar}
+                        // updateCtgy={editCtgy}
+                        />
+                    </div>
+                }
 
 
 
@@ -53,19 +92,3 @@ function CategoryMenu({selectCtgy, menuMode, toggleMenu}) {
 
 export default CategoryMenu;
 
- // {add && <CategoryForm
-                //         toggle={toggleAdd}
-                //         addNewCtgy={addCtgy}
-                //         ctgyName={ctgyName}
-                //         />}
-                
-                // {edit && 
-                // <div>
-                //     {displayCategories}
-                //     <CategoryForm
-                //     ctgyName={ctgyName}
-                //     toggle={toggleEdit}
-                //     updateCtgy={editCtgy}
-                //     />
-                // </div>
-                // }
